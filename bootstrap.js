a6782d3920f9d2943e2b6576f90402544aca1c09
Isabella Santos <bellassnt@gmail.com>
2022-11-24 17:18:57 -0300

    Initial commit

---
+++ bootstrap.js
@@ -0,0 +1,24 @@
+import { LoginHtml } from "./js/pages/login.page.js";
+import { UserSingUpHtml } from "./js/pages/signup.page.js";
+import { UserUpdateHtml } from "./js/pages/update.page.js";
+
+function redirectPages() {
+    const root = document.getElementById('root');
+
+    const Router = {
+        "#login": { component: LoginHtml, path: "#login" },
+        '#signup': { component: UserSingUpHtml, path: '#signup' },
+        '#update': { component: UserUpdateHtml, path: '#update' }
+    }
+
+    const route = Router[window.location.hash] || Router['#404']
+
+    root.innerHTML = null;
+    root.append(route.component());
+    window.history.pushState(null, null, route.path);
+};
+
+window.addEventListener('load', () => {
+    redirectPages();
+    window.addEventListener('hashchange', redirectPages);
+});
